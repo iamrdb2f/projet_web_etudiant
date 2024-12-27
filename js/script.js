12ee01a15efafa7ec4351c856e381f6c3bba16ed
Stephen Thibault <thib.0028@gmail.com>
2024-12-27 22:57:22 +0100

    Refonte complète du site avec Tailwind Css, Ajout de contenu

--- js/script.js
+++ js/script.js
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     backToTopButton.onclick = function () {
-      document.body.scrollTop = 0; // For Safari
-      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      document.body.scrollTop = 0; 
+      document.documentElement.scrollTop = 0;
     };
   }
 
